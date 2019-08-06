b00bf0234d50e325817e001f9b9c401ea2b77622
Logan Barajas <baraj089@csusm.edu>
2019-08-06 11:56:27 -0700

    Fixed organization 2

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ jinja_current_directory = jinja2.Environment(
 
 class RoboTap(webapp2.RequestHandler):
     def get(self):
-<<<<<<< HEAD
         start_template=jinja_current_directory.get_template("Templates/start.html")
-=======
-        start_template=jinja_current_directory.get_template("start.html")
->>>>>>> 1f4db1896e961243cb51f17083760946ede0381c
         self.response.write(start_template.render())
 
 app = webapp2.WSGIApplication([
