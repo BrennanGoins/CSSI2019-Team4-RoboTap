658afcf21c54f8056335018999cf9f6452a2170d
Logan Barajas <baraj089@csusm.edu>
2019-08-07 15:37:30 -0700

    updates login

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import webapp2
 import os
 import jinja2
-import random
+from RoboTapUsersDB import UserData
+
+
 
 jinja_current_directory = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class RoboTap(webapp2.RequestHandler):
+class roboTap(webapp2.RequestHandler):
     def get(self):
-        start_template=jinja_current_directory.get_template("Templates/start.html")
+        start_template=jinja_current_directory.get_template('Templates/start.html')
         self.response.write(start_template.render())
 
+
+class Login(webapp2.RequestHandler):
+    def get(self):
+        start_template=jinja_current_directory.get_template("Templates/login.html")
+        self.response.write(start_template.render())
+
+
+
 app = webapp2.WSGIApplication([
-    ('/', RoboTap)
+    ('/', Login),
+    ('/roboTap', roboTap),
 ], debug=True)
